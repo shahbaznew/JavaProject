ae8fa1ad904cbd7f90ab22aec9539fab7784cfe4
shahbaznew <shahbazayub0@gmail.com>
2024-06-22 03:51:42 +0100

    created first test creating a grid and adding tokens for the correct conditions, checks for horizontal win

--- connect4/src/test/ConnectfourTest.java
+++ connect4/src/test/ConnectfourTest.java
@@ -5,7 +5,15 @@ import static org.junit.Assert.*;
 
 public class ConnectfourTest {
     @Test 
-    public void testhoricontalWin() { 
-        
+    public void testHorizontalWin() { 
+        ConnectFour game = new ConnectFour(6, 7, 4); 
+        game.addToken(0);
+        game.addToken(0);
+        game.addToken(1);
+        game.addToken(1);
+        game.addToken(2);
+        game.addToken(2);
+        boolean winner = game.addToken(3); 
+        assertTrue("Expected horizontal win", winner); 
     }
 }
