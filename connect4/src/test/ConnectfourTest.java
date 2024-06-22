59d045abb4c676f53eb8dde69399b7b47b6207df
shahbaznew <shahbazayub0@gmail.com>
2024-06-22 03:57:14 +0100

    craeting test for diagonal win in the second direction

--- connect4/src/test/ConnectfourTest.java
+++ connect4/src/test/ConnectfourTest.java
@@ -41,6 +41,22 @@ public class ConnectfourTest {
         game.addToken(3);
         game.addToken(3);
         game.addToken(4);
+        boolean winner = game.addToken(3); 
+        assertTrue("Expected Diagonal win", winner); 
+    }
+    @Test 
+    public void testDiagonalWin2() { 
+        ConnectFour game = new ConnectFour(6, 7, 4); 
+        game.addToken(3);
+        game.addToken(2);
+        game.addToken(2);
+        game.addToken(1);
+        game.addToken(1);
+        game.addToken(0);
+        game.addToken(1);
+        game.addToken(0);
+        game.addToken(0);
+        game.addToken(0);
         boolean winner = game.addToken(0); 
         assertTrue("Expected Diagonal win", winner); 
     }
