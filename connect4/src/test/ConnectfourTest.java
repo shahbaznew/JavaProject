3db0e823809c6c7669c9a392ea29d4116c1e36b7
shahbaznew <shahbazayub0@gmail.com>
2024-06-22 03:48:09 +0100

    creating test file and test cases

---
+++ connect4/src/test/ConnectfourTest.java
@@ -0,0 +1,11 @@
+package com.example; 
+
+import org.junit.Test; 
+import static org.junit.Assert.*;
+
+public class ConnectfourTest {
+    @Test 
+    public void testhoricontalWin() { 
+        
+    }
+}
